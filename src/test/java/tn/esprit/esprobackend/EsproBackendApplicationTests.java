cb2de6fa8febac8b784286af601d163e1c55e066
tamim-hmizi <hmizitamim@hotmail.com>
2024-03-20 03:19:44 +0100

    starter project

---
+++ src/test/java/tn/esprit/esprobackend/EsproBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.esprobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsproBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
