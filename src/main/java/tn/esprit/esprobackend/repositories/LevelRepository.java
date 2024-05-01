09aa0d7fdf7bd50c6cde5e59d92ea858f1d6eb23
amenallahsd121 <amenallah.bensmida@esprit.tn>
2024-05-01 23:28:44 +0100

    Amen & Houssem

--- src/main/java/tn/esprit/esprobackend/repositories/LevelRepository.java
+++
@@ -1,9 +0,0 @@
-package tn.esprit.esprobackend.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import tn.esprit.esprobackend.entities.Level;
-
-@Repository
-public interface LevelRepository extends JpaRepository<Level,Long> {
-}
