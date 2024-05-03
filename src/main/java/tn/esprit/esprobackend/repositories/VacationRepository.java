27d14ffeb146a1f13c8571ebf124fd2c441958ef
amenallahsd121 <amenallah.bensmida@esprit.tn>
2024-05-03 11:19:16 +0100

    Feat

--- src/main/java/tn/esprit/esprobackend/repositories/VacationRepository.java
+++
@@ -1,9 +0,0 @@
-package tn.esprit.esprobackend.repositories;
-
-import tn.esprit.esprobackend.entities.Vacation;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VacationRepository extends JpaRepository<Vacation,Long> {
-}
