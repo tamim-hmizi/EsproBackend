0c49267c8acd8983f6cde6d0eb818f5f4319f408
tamim-hmizi <hmizitamim@hotmail.com>
2024-05-07 01:22:28 +0100

    error fixing

--- src/main/java/tn/esprit/esprobackend/controllers/academinRestController.java
+++
@@ -1,5 +0,0 @@
-package tn.esprit.esprobackend.controllers;
-
-
-public class academinRestController {
-}
--- src/main/java/tn/esprit/esprobackend/repositories/acadamicRepo.java
+++
@@ -1,9 +0,0 @@
-package tn.esprit.esprobackend.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import tn.esprit.esprobackend.entities.academicSp;
-
-@Repository
-public interface acadamicRepo extends JpaRepository<academicSp,Long> {
-}
